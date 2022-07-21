f165ede931b3b568030db177323a049cb248b02a
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2022-07-21 11:29:15 -0700

    Update trainer_parties.h

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -1311,7 +1311,7 @@ static const struct TrainerMonItemCustomMoves sParty_Grunt14[] = { // Slateport 
     .species = SPECIES_PALPITOAD,
     .heldItem = ITEM_BERRY_JUICE,	    
     .ability = 2, // Water Absorb
-    .spread = SPREAD_15_IV_0_EV,
+    .spread = SPREAD_15_IV_40_EV,
     .moves = MOVE_BUBBLE_BEAM, MOVE_ACID, MOVE_MUD_SHOT, MOVE_ROUND
     },
     {
@@ -1319,14 +1319,14 @@ static const struct TrainerMonItemCustomMoves sParty_Grunt14[] = { // Slateport 
     .species = SPECIES_BASCULIN,
     .heldItem = ITEM_BERRY_JUICE,	    
     .ability = 1, // Adaptability
-    .spread = SPREAD_15_IV_0_EV,
+    .spread = SPREAD_25_IV_60_EV,
     .moves = MOVE_AQUA_JET, MOVE_CRUNCH, MOVE_TAKE_DOWN, MOVE_REVENGE
     },
     {
     .lvl = 5,
     .species = SPECIES_ZUBAT,
     .heldItem = ITEM_BERRY_JUICE,	    
-    .spread = SPREAD_15_IV_0_EV,
+    .spread = SPREAD_31_IV_80_EV,
     .moves = MOVE_BITE, MOVE_AIR_CUTTER, MOVE_POISON_FANG, MOVE_CONFUSE_RAY
     },	
     {
