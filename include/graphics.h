bb9e1da06dcd72a5df9e1f192dbb118d1d34e6de
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2022-08-23 19:15:24 -0700

    Update graphics.h

--- include/graphics.h
+++ include/graphics.h
@@ -6796,6 +6796,7 @@ extern const u32 gTrainerPalette_Leaf[];
 extern const u32 gTrainerPalette_RubySapphireBrendan[];
 extern const u32 gTrainerPalette_RubySapphireMay[];
 extern const u32 gTrainerPalette_Cynthia[];
+extern const u32 gTrainerPalette_Blue[];
 
 extern const u8 gTrainerBackPic_Brendan[];
 extern const u8 gTrainerBackPic_May[];
