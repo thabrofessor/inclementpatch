06658ed321765d046bcd7bf0c246857bec6d5c7d
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2022-08-30 20:30:43 -0700

    Update opponents.h

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -856,13 +856,22 @@
 #define TRAINER_LEAF                        852
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
-#define TRAINER_BRO                         855
+#define TRAINER_BRO_1                         855
+#define TRAINER_BRO_2                         856
+#define TRAINER_BRO_3                         857
+#define TRAINER_BRO_4                         858
+#define TRAINER_BRO_5                         859
+#define TRAINER_BRO_6                         860
+#define TRAINER_BRO_7                         861
+#define TRAINER_BRO_8                         862
+#define TRAINER_BRO_9                         863
+#define TRAINER_BRO_10                         864
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      855
+#define TRAINERS_COUNT                      864
 #define MAX_TRAINERS_COUNT                  864
 
 
