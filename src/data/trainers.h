43da89aa04ed4f5df93282fcd92164f650e88c6d
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2022-10-07 22:06:00 -0700

    Update trainers.h

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -15421,7 +15421,7 @@ const struct Trainer gTrainers[] = {
         .trainerPic = TRAINER_PIC_ELITE_FOUR_GLACIA,
         .trainerName = _("Glacia"),
         .items = {ITEM_FULL_RESTORE, ITEM_FULL_RESTORE, ITEM_FULL_RESTORE, ITEM_FULL_RESTORE},
-        .doubleBattle = FALSE",
+        .doubleBattle = FALSE,
         .aiFlags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY | AI_FLAG_CHECK_FOE,
         .partySize = ARRAY_COUNT(sParty_GLACIA_4A),
         .party = {.ItemCustomMoves = sParty_GLACIA_4A},
