a2c62e3d1ba79974ffcadeafa84daa363cffa0ac
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2023-01-16 17:30:14 -0700

    Update event_objects.h

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -257,8 +257,9 @@
 #define OBJ_EVENT_GFX_DIANCIE                    253
 #define OBJ_EVENT_GFX_CHANSEY                    254
 #define OBJ_EVENT_GFX_CYNTHIA                    255
+#define OBJ_EVENT_GFX_CYNTHIA                    256
 
-#define NUM_OBJ_EVENT_GFX                        256
+#define NUM_OBJ_EVENT_GFX                        257
 
 
 // These are dynamic object gfx ids.
