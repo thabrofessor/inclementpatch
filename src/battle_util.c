3a656dcfbcd613aec8b69376855e5391b60316cd
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2023-02-18 10:32:01 -0700

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -8664,6 +8664,8 @@ static u32 CalcAttackStat(u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, b
             MulModifier(&modifier, UQ_4_12(2.0));
         break;
     case HOLD_EFFECT_LIGHT_BALL:
+	if (CanEvolve(gBattleMons[battlerDef].species))
+            MulModifier(&modifier, UQ_4_12(1.5));
         if (gBattleMons[battlerAtk].species == SPECIES_PIKACHU)
             MulModifier(&modifier, UQ_4_12(2.0));
         if (GET_BASE_SPECIES_ID(gBattleMons[battlerAtk].species) == SPECIES_RAICHU)
