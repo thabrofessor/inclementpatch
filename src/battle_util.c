fe9cc51c30d8db61f1524fe2bf4d2d18f457a979
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2023-02-18 17:10:27 -0700

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -8664,11 +8664,11 @@ static u32 CalcAttackStat(u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, b
             MulModifier(&modifier, UQ_4_12(2.0));
         break;
     case HOLD_EFFECT_LIGHT_BALL:
-        if (gBattleMons[battlerAtk, battlerDef].species == SPECIES_PIKACHU)
+        if (gBattleMons[battlerAtk].species == SPECIES_PIKACHU)
             MulModifier(&modifier, UQ_4_12(2.0));
         if (GET_BASE_SPECIES_ID(gBattleMons[battlerAtk].species) == SPECIES_RAICHU)
             MulModifier(&modifier, UQ_4_12(1.5));
-	if (gBattleMons[battlerAtk] && [battlerDef].species == SPECIES_PICHU)
+	if (gBattleMons[battlerAtk].species == SPECIES_PICHU)
             MulModifier(&modifier, UQ_4_12(2.0))
         break;
     case HOLD_EFFECT_CHOICE_BAND:
