79ae205fb6044d174b5a4db9ca9a86fb99de9c21
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2023-03-29 15:34:48 -0700

    Update pokemon.h

--- include/pokemon.h
+++ include/pokemon.h
@@ -53,12 +53,12 @@ struct PokemonSubstruct3
  /* 0x03 */ u16 nature:5;
  /* 0x03 */ u16 otGender:1;
 
- /* 0x04 */ u32 hpIV:9;
- /* 0x04 */ u32 attackIV:9;
- /* 0x05 */ u32 defenseIV:9;
- /* 0x05 */ u32 speedIV:9;
- /* 0x05 */ u32 spAttackIV:9;
- /* 0x06 */ u32 spDefenseIV:9;
+ /* 0x04 */ u32 hpIV:6;
+ /* 0x04 */ u32 attackIV:6;
+ /* 0x05 */ u32 defenseIV:6;
+ /* 0x05 */ u32 speedIV:6;
+ /* 0x05 */ u32 spAttackIV:6;
+ /* 0x06 */ u32 spDefenseIV:6;
  /* 0x07 */ u32 isEgg:1;
  /* 0x07 */ u32 UnusedBit:1; 
 
