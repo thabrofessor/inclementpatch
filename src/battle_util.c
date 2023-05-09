74d440001617d2f643bbf5def2e7a11b17200f16
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2023-05-08 19:48:00 -0700

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -4717,20 +4717,20 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
                     BattleScriptPushCursorAndCallback(BattleScript_BadDreamsActivates);
                     effect++;
                 }
-                break;
-		/*	    
+                break;	    
             SOLAR_POWER_HP_DROP:
             case ABILITY_SOLAR_POWER:
                 if (IsBattlerWeatherAffected(battler, WEATHER_SUN_ANY))
                 {
+			//
                     BattleScriptPushCursorAndCallback(BattleScript_SolarPowerActivates);
                     gBattleMoveDamage = gBattleMons[battler].maxHP / 8;
                     if (gBattleMoveDamage == 0)
                         gBattleMoveDamage = 1;
                     effect++;
+			 gBattleMoveDamage = 0; //
                 }
                 break;
-		*/
             case ABILITY_HEALER:
                 gBattleScripting.battler = BATTLE_PARTNER(battler);
                 if (IsBattlerAlive(gBattleScripting.battler)
