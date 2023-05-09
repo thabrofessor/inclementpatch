99f3a2abeed438c3d7b2a359befe6adb3f16f12e
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2023-05-09 17:54:27 -0600

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -4073,7 +4073,7 @@ void CreateBoxMon(struct BoxPokemon *boxMon, u16 species, u8 level, u8 fixedIV, 
         if (CheckBagHasItem(ITEM_SHINY_CHARM, 1))
         {
             u32 shinyValue;
-            u32 rolls = 3333;
+            u32 rolls = 7777;
             do
             {
                 personality = Random32();
