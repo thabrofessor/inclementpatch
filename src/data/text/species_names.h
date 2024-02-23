2b9c9401dd3e03bcd6c9d2e33cdea2bf9ef4810b
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2024-02-23 07:28:51 -0700

    Update species_names.h

--- src/data/text/species_names.h
+++ src/data/text/species_names.h
@@ -1,4 +1,4 @@
-const u8 gSpeciesNames[][POKEMON_NAME_LENGTH + 5] = {
+const u8 gSpeciesNames[][POKEMON_NAME_LENGTH + 1] = {
     [SPECIES_NONE] = _("??????????"),
     [SPECIES_BULBASAUR] = _("Bulbasaur"),
     [SPECIES_IVYSAUR] = _("Ivysaur"),
