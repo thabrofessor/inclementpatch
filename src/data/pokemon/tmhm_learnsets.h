c93131db8fd7176075a13b4f807bd5cc09e554c2
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2024-02-27 22:02:54 -0700

    Update tmhm_learnsets.h

--- src/data/pokemon/tmhm_learnsets.h
+++ src/data/pokemon/tmhm_learnsets.h
@@ -38310,7 +38310,11 @@ const u32 gTMHMLearnsets[][4] =
                                         | TMHM1(TM25_THUNDER),
                                           TMHM2(TM90_SUBSTITUTE)
                                         | TMHM2(TM94_SUCKER_PUNCH)),
-    [SPECIES_BASCULEGION]   = TMHM_LEARNSET (TMHM1(TM24_THUNDERBOLT)
+    [SPECIES_BASCULEGION_MALE]   = TMHM_LEARNSET (TMHM1(TM24_THUNDERBOLT)
+                                        | TMHM1(TM25_THUNDER),
+                                          TMHM2(TM90_SUBSTITUTE)
+                                        | TMHM2(TM94_SUCKER_PUNCH)),
+    [SPECIES_BASCULEGION_FEMALE]   = TMHM_LEARNSET (TMHM1(TM24_THUNDERBOLT)
                                         | TMHM1(TM25_THUNDER),
                                           TMHM2(TM90_SUBSTITUTE)
                                         | TMHM2(TM94_SUCKER_PUNCH)),
