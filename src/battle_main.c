61925e5d676b803e469af92ffa53adbcd825c522
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2024-03-10 17:41:03 -0600

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -647,35 +647,35 @@ static void CB2_InitBattleInternal(void)
     gSaveBlock2Ptr->frontier.disableRecordBattle = FALSE;
 
     for (i = 0; i < PARTY_SIZE; i++)
- //   {
+   {
         AdjustFriendship(&gPlayerParty[i], FRIENDSHIP_EVENT_LEAGUE_BATTLE);
       
  //  Apply enemy buff
   //  {
-    //    if (GetMonData(&gEnemyParty[i], MON_DATA_HELD_ITEM) == ITEM_WOOD_MAIL)
-   //      {
-    //         u8 points    = 30;
-    //         u8 currHp    = GetMonData(&gEnemyParty[i], MON_DATA_HP);
-   //          u8 currAtk   = GetMonData(&gEnemyParty[i], MON_DATA_ATK);
-   //          u8 currDef   = GetMonData(&gEnemyParty[i], MON_DATA_DEF);
-   //          u8 currSpeed = GetMonData(&gEnemyParty[i], MON_DATA_SPEED);
-   //          u8 currSpAtk = GetMonData(&gEnemyParty[i], MON_DATA_SPATK);
-   //          u8 currSpDef = GetMonData(&gEnemyParty[i], MON_DATA_SPDEF);
-   //          u16 newHp     = currHp + points;
-   //          u16 newAtk    = currAtk + points;
-   //          u16 newDef    = currDef + points;
-   //          u16 newSpeed  = currSpeed + points;
-   //          u16 newSpAtk  = currSpAtk + points;
-   //          u16 newSpDef  = currSpDef + points;
-   //     
-   //          SetMonData(&gEnemyParty[i], MON_DATA_HP, &newHp);
-   //          SetMonData(&gEnemyParty[i], MON_DATA_ATK, &newAtk);
-   //          SetMonData(&gEnemyParty[i], MON_DATA_DEF, &newDef);
-   //          SetMonData(&gEnemyParty[i], MON_DATA_SPEED, &newSpeed);
-    //         SetMonData(&gEnemyParty[i], MON_DATA_SPATK, &newSpAtk);
-   //          SetMonData(&gEnemyParty[i], MON_DATA_SPDEF, &newSpDef);
-   //      }
-   // }
+        if (GetMonData(&gEnemyParty[i], MON_DATA_HELD_ITEM) == ITEM_WOOD_MAIL)
+        {
+            u32 points    = 30;
+             u32 currHp    = GetMonData(&gEnemyParty[i], MON_DATA_HP);
+             u32 currAtk   = GetMonData(&gEnemyParty[i], MON_DATA_ATK);
+             u32 currDef   = GetMonData(&gEnemyParty[i], MON_DATA_DEF);
+             u32 currSpeed = GetMonData(&gEnemyParty[i], MON_DATA_SPEED);
+             u32 currSpAtk = GetMonData(&gEnemyParty[i], MON_DATA_SPATK);
+             u32 currSpDef = GetMonData(&gEnemyParty[i], MON_DATA_SPDEF);
+             u32 newHp     = currHp + points;
+             u32 newAtk    = currAtk + points;
+             u32 newDef    = currDef + points;
+             u32 newSpeed  = currSpeed + points;
+             u32 newSpAtk  = currSpAtk + points;
+             u32 newSpDef  = currSpDef + points;
+        
+             SetMonData(&gEnemyParty[i], MON_DATA_HP, &newHp);
+             SetMonData(&gEnemyParty[i], MON_DATA_ATK, &newAtk);
+             SetMonData(&gEnemyParty[i], MON_DATA_DEF, &newDef);
+             SetMonData(&gEnemyParty[i], MON_DATA_SPEED, &newSpeed);
+             SetMonData(&gEnemyParty[i], MON_DATA_SPATK, &newSpAtk);
+             SetMonData(&gEnemyParty[i], MON_DATA_SPDEF, &newSpDef);
+         }
+    }
     gBattleCommunication[MULTIUSE_STATE] = 0;
 }
 
