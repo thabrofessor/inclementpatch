7e8cfe900ecf103def80074492196571680d74ca
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2024-03-10 21:23:12 -0600

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -662,8 +662,46 @@ static void CB2_InitBattleInternal(void)
  //            u32 currSpeed = GetMonData(&gEnemyParty[i], MON_DATA_SPEED);
  //            u32 currSpAtk = GetMonData(&gEnemyParty[i], MON_DATA_SPATK);
  //            u32 currSpDef = GetMonData(&gEnemyParty[i], MON_DATA_SPDEF);
-                       u32 newHp = GetMonData(&gEnemyParty[i], MON_DATA_HP) + points;
-                     u32 newAtk = GetMonData(&gEnemyParty[i], MON_DATA_ATK) + points;
+                    u32 newHp = GetMonData(&gEnemyParty[i], MON_DATA_HP) + points;
+                    u32 newAtk = GetMonData(&gEnemyParty[i], MON_DATA_ATK) + points;
+                    u32 newDef = GetMonData(&gEnemyParty[i], MON_DATA_DEF) + points;
+                    u32 newSpeed = GetMonData(&gEnemyParty[i], MON_DATA_SPEED) + points;
+                    u32 newSpAtk = GetMonData(&gEnemyParty[i], MON_DATA_SPATK) + points;
+                    u32 newSpDef = GetMonData(&gEnemyParty[i], MON_DATA_SPDEF) + points;
+ //            u32 newHp     = currHp + points;
+ //            u32 newAtk    = currAtk + points;
+ //            u32 newDef    = currDef + points;
+ //            u32 newSpeed  = currSpeed + points;
+ //            u32 newSpAtk  = currSpAtk + points;
+ //            u32 newSpDef  = currSpDef + points;
+        
+             SetMonData(&gEnemyParty[i], MON_DATA_HP, &newHp);
+             SetMonData(&gEnemyParty[i], MON_DATA_ATK, &newAtk);
+             SetMonData(&gEnemyParty[i], MON_DATA_DEF, &newDef);
+             SetMonData(&gEnemyParty[i], MON_DATA_SPEED, &newSpeed);
+             SetMonData(&gEnemyParty[i], MON_DATA_SPATK, &newSpAtk);
+             SetMonData(&gEnemyParty[i], MON_DATA_SPDEF, &newSpDef);
+         }
+    }
+
+        for (i = 0; i < PARTY_SIZE; i++)
+   {
+        AdjustFriendship(&gPlayerParty[i], FRIENDSHIP_EVENT_LEAGUE_BATTLE);
+      
+ //  Apply enemy buff
+  //  {
+
+        if (GetMonData(&gEnemyParty[i], MON_DATA_HELD_ITEM) == ITEM_ORAN_BERRY)
+        {
+            u32 points    = 30;
+ //            u32 currHp    = GetMonData(&gEnemyParty[i], MON_DATA_HP);
+ //            u32 currAtk   = GetMonData(&gEnemyParty[i], MON_DATA_ATK);
+ //            u32 currDef   = GetMonData(&gEnemyParty[i], MON_DATA_DEF);
+ //            u32 currSpeed = GetMonData(&gEnemyParty[i], MON_DATA_SPEED);
+ //            u32 currSpAtk = GetMonData(&gEnemyParty[i], MON_DATA_SPATK);
+ //            u32 currSpDef = GetMonData(&gEnemyParty[i], MON_DATA_SPDEF);
+                    u32 newHp = GetMonData(&gEnemyParty[i], MON_DATA_HP) + points;
+                    u32 newAtk = GetMonData(&gEnemyParty[i], MON_DATA_ATK) + points;
                     u32 newDef = GetMonData(&gEnemyParty[i], MON_DATA_DEF) + points;
                     u32 newSpeed = GetMonData(&gEnemyParty[i], MON_DATA_SPEED) + points;
                     u32 newSpAtk = GetMonData(&gEnemyParty[i], MON_DATA_SPATK) + points;
