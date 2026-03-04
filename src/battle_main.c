9a3ec45608b4100883905922ebc21730cc157430
thabrofessor <73491159+thabrofessor@users.noreply.github.com>
2026-03-04 15:02:52 -0700

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -645,7 +645,7 @@ static void CB2_InitBattleInternal(void)
 
     gMain.inBattle = TRUE;
     gSaveBlock2Ptr->frontier.disableRecordBattle = FALSE;
-
+{
     for (i = 0; i < PARTY_SIZE; i++)
         AdjustFriendship(&gPlayerParty[i], FRIENDSHIP_EVENT_LEAGUE_BATTLE);
 
